6f598a66fff5fcebd1484930ef82626d8533d200
Ahmed khaled <ahmedkh8907@gmail.com>
2024-08-23 01:40:41 +0300

    creating sign up use case

---
+++ lib/domain/usecases/signup_usecase.dart
@@ -0,0 +1,13 @@
+import 'package:clot_shop/core/usecases/usecase.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../data/models/user_creation_req.dart';
+import '../../service_locator.dart';
+import '../auth/repos/auth_repo.dart';
+
+class SignupUsecase extends Usecase<Either, UserCreationReq> {
+  @override
+  Future<Either> call({UserCreationReq ? param}) async{
+   return await sl<AuthRepo>().signup(param!);
+  }
+}
--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,6 +1,7 @@
 import 'package:clot_shop/data/repo/auth_repo_impl.dart';
 import 'package:clot_shop/data/source/auth_firebase_source.dart';
 import 'package:clot_shop/domain/auth/repos/auth_repo.dart';
+import 'package:clot_shop/domain/usecases/signup_usecase.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
@@ -15,4 +16,7 @@ void serviceLocator() {
     AuthRepoImpl(),
   );
   // use cases
+  sl.registerSingleton<SignupUsecase>(
+    SignupUsecase(),
+  );
 }

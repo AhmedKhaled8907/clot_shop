3f1818be410ff4e41e791ec564ff78e72e58f3ea
Ahmed khaled <ahmedkh8907@gmail.com>
2024-08-22 18:43:36 +0300

    adding assets class

---
+++ lib/core/configs/assets.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for imagesOrderPlaced
+  /// assets/images/order_placed.png
+  static const String imagesOrderPlaced = "assets/images/order_placed.png";
+
+  /// Assets for imagesProfile
+  /// assets/images/profile.png
+  static const String imagesProfile = "assets/images/profile.png";
+
+  /// Assets for vectorsBag
+  /// assets/vectors/bag.svg
+  static const String vectorsBag = "assets/vectors/bag.svg";
+
+  /// Assets for vectorsCartBag
+  /// assets/vectors/cart_bag.svg
+  static const String vectorsCartBag = "assets/vectors/cart_bag.svg";
+
+  /// Assets for vectorsEmailSending
+  /// assets/vectors/email_sending.svg
+  static const String vectorsEmailSending = "assets/vectors/email_sending.svg";
+
+  /// Assets for vectorsLogo
+  /// assets/vectors/logo.svg
+  static const String vectorsLogo = "assets/vectors/logo.svg";
+
+  /// Assets for vectorsNotFound
+  /// assets/vectors/not_found.svg
+  static const String vectorsNotFound = "assets/vectors/not_found.svg";
+
+  /// Assets for vectorsSearch
+  /// assets/vectors/search.svg
+  static const String vectorsSearch = "assets/vectors/search.svg";
+}
